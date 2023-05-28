b83f995ccbae7db12e54ed313a0982876d3086ba
Ahmed Ibrahim <aosama@gmail.com>
2023-05-28 22:15:52 +0000

    maven test dependencies and some refactoring

---
+++ src/test/java/org/primefaces/showcase/CustomerServiceTest.java
@@ -0,0 +1,21 @@
+package org.testing;
+
+import jakarta.inject.Inject;
+import org.primefaces.showcase.service.CustomerService;
+
+
+public class CustomerServiceTest {
+
+    //testing getCustomers from customerService
+
+    @Inject
+    CustomerService customerService;
+
+
+
+    private void testGetCustomer()
+    {
+
+    }
+
+}
--- src/test/java/org/testing/BasicTest.java
+++
@@ -1,6 +0,0 @@
-package org.testing;
-
-public class BasicTest {
-    
-        
-}
